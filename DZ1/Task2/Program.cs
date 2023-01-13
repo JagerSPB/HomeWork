b15d6f88b8e9fd8196a9626d6b648f4a026e0d0d
JagerSPB <Jage7@yandex.ru>
2023-01-14 00:58:21 +0300

    Добавил первое домашнее задание

---
+++ DZ1/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*Задача: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1 > number2 | number1 > number3)
+{
+  int max = number1;
+  
+}
+if (number2 > number1 | number2 > number3)
+{
+  int max = number2;
+  
+}
+if (number3 > number1 | number3 > number2)
+{
+  int max = number3;
+  Console.WriteLine(max);
+}
