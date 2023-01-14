df8b50f0e5183510eecf05a6ba874d786f01b1ff
JagerSPB <Jage7@yandex.ru>
2023-01-14 14:27:09 +0300

    задача 3  решена

--- DZ1/Task3/Program.cs
+++ DZ1/Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number%2) == 0)
+{
+  Console.WriteLine("Это чётное число");
+}
+else
+Console.WriteLine("Это НЕ чётное число");
