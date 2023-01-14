908c3d16a4b0bc82579444305be085df6eb2d92c
JagerSPB <Jage7@yandex.ru>
2023-01-14 13:50:56 +0300

    вторая задача решена

--- DZ1/Task2/Program.cs
+++ DZ1/Task2/Program.cs
@@ -1,5 +1,4 @@
-﻿
-/*Задача: Напишите программу, которая принимает на вход три числа 
+﻿/*Задача: Напишите программу, которая принимает на вход три числа 
 и выдаёт максимальное из этих чисел.
 
 2, 3, 7 -> 7
@@ -13,18 +12,15 @@ int number2 = int.Parse(Console.ReadLine());
 Console.Write("Введите число 3: ");
 int number3 = int.Parse(Console.ReadLine());
 
-if (number1 > number2 | number1 > number3)
+if ((number1 > number2) & (number1 > number3) )
 {
-  int max = number1;
-  
+  Console.WriteLine(number1);
 }
-if (number2 > number1 | number2 > number3)
+if ((number2 > number1) & (number2 > number3) )
 {
-  int max = number2;
-  
+  Console.WriteLine(number2);
 }
-if (number3 > number1 | number3 > number2)
+else if ((number3 > number1) & (number3 > number2) )
 {
-  int max = number3;
-  Console.WriteLine(max);
+  Console.WriteLine(number3);
 }
