6e939e63b029ff918dbf587188d2bf5c0ee9552c
JagerSPB <Jage7@yandex.ru>
2023-01-17 20:09:14 +0300

    try to add folder

---
+++ DZ2/Task2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+//  456 -> 5
+//  782 -> 8
+//  918 -> 1
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int numA = num / 100;
+int numC = num % 10;
+int numB = num - (((numA*100) + numC));
+int result = numB / 10;
+Console.WriteLine($"-> {result} ");
